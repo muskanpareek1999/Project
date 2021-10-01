ba3a08f5559ad8f632896491c0823d7a9b6ee57f
Muskan Pareek <42691705+muskanpareek1999@users.noreply.github.com>
2021-10-01 22:09:18 +0530

    update

--- webapp/src/main/webapp/demo.js
+++ webapp/src/main/webapp/demo.js
@@ -1,33 +1,32 @@
-const $petals = document.querySelectorAll('.petal')
-let color = 0
-const mouse = {
-  x: 1,
-  y: 1
-}
-
-const handleMouse = (e) => {
-  const x = e.clientX || e.touches[0].clientX
-  const y = e.clientY || e.touches[0].clientY
-  mouse.x = x / window.innerWidth
-  mouse.y = y / window.innerHeight
-}
-['mousemove', 'touchstart', 'touchmove'].forEach(e => window.addEventListener(e, handleMouse))
-
-const lerp = (a, b, n) => (1 - n) * a + n * b
-
-const animate = () => {
-  $petals.forEach((b, ind) => {
-    const i = ind / $petals.length
-    const x = Math.sin(i * Math.PI * 2) * 48
-    const y = -Math.cos(i * Math.PI * 2) * 48
-    color = lerp(color, mouse.y * 360, .004)
-    const style = {
-      transform: `translate(${x}px, ${y}px) rotate(${i * 360 + mouse.y * 360}deg) scale(${0.5 + mouse.x * 0.5})`,
-      borderRadius: `${50 + mouse.y * 50}% 0 ${50 + mouse.x * 50}% 50%`
+carousel = (function(){
+  var imagecounter = document.querySelector('.imagecounter');
+  var box = document.querySelector('.imagebox');
+  var next = box.querySelector('.content');
+  var items = box.querySelectorAll('.content li');
+  var counter = 0;
+  var amount = items.length;
+  var current = items[0];
+  box.classList.add('active');
+  
+  function navigate(direction) {
+    current.classList.remove('current');
+    counter = counter + direction;
+    if (direction === -1 && 
+        counter < 0) { 
+      counter = amount - 1; 
     }
-    Object.assign(b.style, style)
-    b.style.setProperty('--color', `hsl(${Math.floor(color)}, 60%, 50%)`)
-  })
-  requestAnimationFrame(animate)
-}
-animate()
+    if (direction === 1 && 
+        !items[counter]) { 
+      counter = 0;
+    }
+    current = items[counter];
+    current.classList.add('current');
+    
+  }
+  next.addEventListener('click', function(ev) {
+    navigate(1);
+    imagecounter.innerHTML = "["+(counter+1) +"/"+amount+"]";
+    
+  });
+  navigate(0);
+})();
